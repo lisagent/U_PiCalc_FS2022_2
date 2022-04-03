fed6ed5c437585cc7369cccaee5d143eed4f5b84
Lisa <lisa.gent1@googelmail.com>
2022-04-03 16:46:18 +0200

    PC

--- U_PiCalc/main.c
+++ U_PiCalc/main.c
@@ -30,11 +30,16 @@
 
 #include "ButtonHandler.h"
 
-
+float pi4;
+float pi;
+float pis;
+float pi_l;
+float pi_s;
 
 
 void controllerTask(void* pvParameters);
-void vPi_RechnerTast(void* pvParameters);
+void leibniz_task(void* pvParameters);
+void Somayaji_task(void* pvParameters);
 
 int main(void)
 {
@@ -42,7 +47,8 @@ int main(void)
 	vInitDisplay();
 	
 	xTaskCreate( controllerTask, (const char *) "control_tsk", configMINIMAL_STACK_SIZE+150, NULL, 3, NULL);
-	xTaskCreate( vPi_RechnerTast, (const char *) "control_tsk", configMINIMAL_STACK_SIZE+150, NULL, 2, NULL);
+	xTaskCreate( leibniz_task, (const char *) "leibniz_tsk", configMINIMAL_STACK_SIZE+150, NULL, 1, NULL);
+	xTaskCreate( Somayaji_task, (const char *) "Somayaji_tsk", configMINIMAL_STACK_SIZE+150, NULL, 1, NULL);
 
 	vDisplayClear();
 	vDisplayWriteStringAtPos(0,0,"PI-Calc FS2022");
@@ -51,30 +57,77 @@ int main(void)
 	return 0;
 }
 
-void vPi_RechnerTast(void* pvParameters){
-	sign = 1;
-	pi_4 = 0;
-for k in range(0,8) :
-    pi_4 += sign / (2*k+1);
-    print(pi_4, sign/(2*k+1), sign);
-    sign = sign * (-1);
-	print(pi_4*4);
+void leibniz_task(void* pvParameters){
+	pi4 = 1;
+	uint32_t zaehler = 3;
+	float pi = 0;
+ for (;;) {
+	 pi4 = pi4-(1.0/zaehler);
+	 zaehler += 2;
+	 pi4 = pi4 +(1.0/zaehler);
+	 zaehler += 2;
+	 pi_l = pi4*4;
+	// informiere_task();
+	 //warte_hier();
+ }
 }
 
+void Somayaji_task(void* pvParameters){
+	pis = 3;
+	uint32_t zaehler_s = 3;
+ for (;;) {	
+	 pis = pis+(4.0/(pow(zaehler_s,3)-zaehler_s));
+	 zaehler_s += 2;
+	 pis = pis-(4.0/(pow(zaehler_s,3)-zaehler_s));
+	 zaehler_s += 2;
+	 pi_s = pis;	
+	// informiere_task();
+	 //warte_hier();
+ }
+}
+
+
 void controllerTask(void* pvParameters) {
+	char pistring[12];
+	uint32_t fivehundertmillisecondscounter = 0;
 	initButtons();
 	for(;;) {
 		updateButtons();
-		if(getButtonPress(BUTTON1) == SHORT_PRESSED) {
-			char pistring[12];
-			sprintf(&pistring[0], "PI: %.8f", M_PI);
+		
+		if (fivehundertmillisecondscounter == 0){
+			vDisplayClear();
+			sprintf(&pistring[0], "PI_L: %.8f", pi_l);
 			vDisplayWriteStringAtPos(1,0, "%s", pistring);
+			sprintf(&pistring[0], "PI_S: %.8f", pi_s);
+			vDisplayWriteStringAtPos(2,0, "%s", pistring);
+			fivehundertmillisecondscounter = 50;
+		} else {
+			fivehundertmillisecondscounter --;
 		}
+		////if(getButtonPress(BUTTON1) == SHORT_PRESSED) {
+			////char pistring[12];
+			////sprintf(&pistring[0], "PI: %.8f", M_PI);
+			////vDisplayWriteStringAtPos(1,0, "%s", pistring);
+		////}
 		if(getButtonPress(BUTTON2) == SHORT_PRESSED) {
-			
+					//if (fivehundertmillisecondscounter == 0){
+			//vDisplayClear();
+			//sprintf(&pistring[0], "PI: %.8f", pi_l);
+			//vDisplayWriteStringAtPos(1,0, "%s", pistring);
+			//fivehundertmillisecondscounter = 50;
+		//} else {
+			//fivehundertmillisecondscounter --;
+		//}
 		}
 		if(getButtonPress(BUTTON3) == SHORT_PRESSED) {
-			
+					//if (fivehundertmillisecondscounter == 0){
+			//vDisplayClear();
+			//sprintf(&pistring[0], "PI: %.8f", pi_s);
+			//vDisplayWriteStringAtPos(1,0, "%s", pistring);
+			//fivehundertmillisecondscounter = 50;
+		//} else {
+			//fivehundertmillisecondscounter --;
+		//}
 		}
 		if(getButtonPress(BUTTON4) == SHORT_PRESSED) {
 			

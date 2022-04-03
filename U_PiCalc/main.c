f128d5a17c1727958d2ba3f116bc871483c0b49b
lisagent <lisa.gent1@googlemail.com>
2022-04-03 18:10:18 +0200

    Lappi

--- U_PiCalc/main.c
+++ U_PiCalc/main.c
@@ -29,26 +29,40 @@
 #include "NHD0420Driver.h"
 
 #include "ButtonHandler.h"
+#include "avr_f64.h"
 
 float pi4;
 float pi;
-float pis;
+float piN;
 float pi_l;
-float pi_s;
+float pi_n;
+int action;
 
+//Eventgroup erstellen
+#define StartLeibniz	1<<0
+#define StopLeibniz		1<<1
+#define StartNilakantha 1<<2
+#define StopNilakantha	1<<3
 
+#define PiCalc_Reset	0x03FC
+EventGroupHandle_t EventGroupPiCalc;
+
+//Forwardsdecleration Tasks
 void controllerTask(void* pvParameters);
 void leibniz_task(void* pvParameters);
-void Somayaji_task(void* pvParameters);
+void Nilakantha_task(void* pvParameters);
 
 int main(void)
 {
     vInitClock();
 	vInitDisplay();
 	
+	EventGroupPiCalc = xEventGroupCreate();
+	
+	//die unterschiedlichen Tasks
 	xTaskCreate( controllerTask, (const char *) "control_tsk", configMINIMAL_STACK_SIZE+150, NULL, 3, NULL);
 	xTaskCreate( leibniz_task, (const char *) "leibniz_tsk", configMINIMAL_STACK_SIZE+150, NULL, 1, NULL);
-	xTaskCreate( Somayaji_task, (const char *) "Somayaji_tsk", configMINIMAL_STACK_SIZE+150, NULL, 1, NULL);
+	xTaskCreate( Nilakantha_task, (const char *) "Somayaji_tsk", configMINIMAL_STACK_SIZE+150, NULL, 1, NULL);
 
 	vDisplayClear();
 	vDisplayWriteStringAtPos(0,0,"PI-Calc FS2022");
@@ -62,25 +76,28 @@ void leibniz_task(void* pvParameters){
 	uint32_t zaehler = 3;
 	float pi = 0;
  for (;;) {
-	 pi4 = pi4-(1.0/zaehler);
-	 zaehler += 2;
-	 pi4 = pi4 +(1.0/zaehler);
-	 zaehler += 2;
-	 pi_l = pi4*4;
+	action = xEventGroupWaitBits (EventGroupPiCalc, StartLeibniz,pdTRUE,pdFALSE, portMAX_DELAY);
+	if ((action & StartLeibniz) !=0 ){
+		pi4 = pi4-(1.0/zaehler);
+		 zaehler += 2;
+		pi4 = pi4 +(1.0/zaehler);
+		zaehler += 2;
+		pi_l = pi4*4;
+	}
 	// informiere_task();
 	 //warte_hier();
  }
 }
 
-void Somayaji_task(void* pvParameters){
-	pis = 3;
-	uint32_t zaehler_s = 3;
+void Nilakantha_task(void* pvParameters){
+	piN = 3;
+	uint64_t zaehler_s = 3;
  for (;;) {	
-	 pis = pis+(4.0/(pow(zaehler_s,3)-zaehler_s));
+	 piN = piN+(4.0/(pow(zaehler_s,3)-zaehler_s));
 	 zaehler_s += 2;
-	 pis = pis-(4.0/(pow(zaehler_s,3)-zaehler_s));
+	 piN = piN-(4.0/(pow(zaehler_s,3)-zaehler_s));
 	 zaehler_s += 2;
-	 pi_s = pis;	
+	 pi_n = piN;	
 	// informiere_task();
 	 //warte_hier();
  }
@@ -94,21 +111,28 @@ void controllerTask(void* pvParameters) {
 	for(;;) {
 		updateButtons();
 		
-		if (fivehundertmillisecondscounter == 0){
-			vDisplayClear();
-			sprintf(&pistring[0], "PI_L: %.8f", pi_l);
-			vDisplayWriteStringAtPos(1,0, "%s", pistring);
-			sprintf(&pistring[0], "PI_S: %.8f", pi_s);
-			vDisplayWriteStringAtPos(2,0, "%s", pistring);
-			fivehundertmillisecondscounter = 50;
-		} else {
-			fivehundertmillisecondscounter --;
+		//if (fivehundertmillisecondscounter == 0){
+			//vDisplayClear();
+			//sprintf(&pistring[0], "PI_L: %.8f", pi_l);
+			//vDisplayWriteStringAtPos(1,0, "%s", pistring);
+			//sprintf(&pistring[0], "PI_N: %.8f", pi_n);
+			//vDisplayWriteStringAtPos(2,0, "%s", pistring);
+			//fivehundertmillisecondscounter = 50;
+		//} else {
+			//fivehundertmillisecondscounter --;
+		//}
+		if(getButtonPress(BUTTON1) == SHORT_PRESSED) {
+				vDisplayClear();
+				sprintf(&pistring[0], "PI_L: %.8f", pi_l);
+				vDisplayWriteStringAtPos(0,0, "Leibniz-Task");
+				vDisplayWriteStringAtPos(1,0, "%s", pistring);			
+			xEventGroupSetBits(EventGroupPiCalc,StartLeibniz);
+
+			
+			//char pistring[12];
+			//sprintf(&pistring[0], "PI: %.8f", M_PI);
+			//vDisplayWriteStringAtPos(1,0, "%s", pistring);
 		}
-		////if(getButtonPress(BUTTON1) == SHORT_PRESSED) {
-			////char pistring[12];
-			////sprintf(&pistring[0], "PI: %.8f", M_PI);
-			////vDisplayWriteStringAtPos(1,0, "%s", pistring);
-		////}
 		if(getButtonPress(BUTTON2) == SHORT_PRESSED) {
 					//if (fivehundertmillisecondscounter == 0){
 			//vDisplayClear();

61111733238063e33326465ffc0ebbd2ce99bc7c
lisagent <lisa.gent1@googlemail.com>
2022-04-16 16:56:34 +0200

    lappi

--- U_PiCalc/main.c
+++ U_PiCalc/main.c
@@ -31,14 +31,17 @@
 #include "ButtonHandler.h"
 #include "avr_f64.h"
 
+
 float pi4;
 float pi;
 float pi_x;
 float piN;
 float pi_l;
 float pi_n;
-uint32_t datenuebertragung;
-TickType_t xStart, xEnd, xDifference;
+float datenuebertragung;
+float pi_nachkomma;
+TickType_t Zeit_Start, Zeit_End, Zeit_Difference;
+TickType_t  Zeit_Start_n, Zeit_End_n, Zeit_Difference_n;
 
 //Eventgroup erstellen
 EventGroupHandle_t EventGroupPiCalc;
@@ -47,18 +50,15 @@ EventGroupHandle_t EventGroupPiCalc;
 #define ResetTask			1<<2
 #define WechsleLeibniz		1<<3
 #define WechsleNilakantha	1<<4
-//#define UebertragungsReset	1<<5
-//#define Datensperren		1<<6
-//#define daten_bereit		1<<7
-//#define daten_cleared		1<<8
-#define PiCalc_Reset	0x03FC
+#define UebertragungsReset	1<<5
+#define Datensperren		1<<6
+#define daten_bereit		1<<7
+#define daten_cleared		1<<8
 
-
-//Forwardsdecleration Tasks
+//forward declaration für die Tasks
 void controllerTask(void* pvParameters);
 void leibniz_task(void* pvParameters);
 void Nilakantha_task(void* pvParameters);
-void vButtonTask(void *pvParameters);
 
 int main(void)
 {
@@ -66,13 +66,11 @@ int main(void)
 	vInitDisplay();
 	
 	EventGroupPiCalc = xEventGroupCreate();
-	//MeinMutex = xSemaphoreCreateMutex();
-	
+
 	//die unterschiedlichen Tasks
 	xTaskCreate( controllerTask, (const char *) "control_tsk", configMINIMAL_STACK_SIZE+150, NULL, 3, NULL);
 	xTaskCreate( leibniz_task, (const char *) "leibniz_tsk", configMINIMAL_STACK_SIZE+150, NULL, 1, NULL);
 	xTaskCreate( Nilakantha_task, (const char *) "Nilakantha_tsk", configMINIMAL_STACK_SIZE+150, NULL, 1, NULL);
-//	xTaskCreate( vButtonTask, (const char *) "buttonTask", configMINIMAL_STACK_SIZE+50, NULL, 1, NULL);
 
 	vDisplayClear();
 	vDisplayWriteStringAtPos(0,0,"PI-Calc FS2022");
@@ -84,14 +82,11 @@ int main(void)
 void leibniz_task(void* pvParameters){
 	pi4 = 1;
 	uint32_t zaehler = 3;
-	//float pi = 0;
-		//xStart = xTaskGetTickCount();
+	pi_nachkomma = 3.14160;
  for (;;) {
-	//action = xEventGroupWaitBits (EventGroupPiCalc, StartLeibniz,pdTRUE,pdFALSE, portMAX_DELAY);
-	//if ((action & StartLeibniz) !=0 ){
 	if((xEventGroupGetBits(EventGroupPiCalc)&ResetTask)){
-		//pi4 = 1;
-		//uint32_t zaehler = 3;
+		pi4 = 0;
+		uint32_t zaehler = 3;
 		pi_l = 0;
 	} 
 	if((xEventGroupGetBits(EventGroupPiCalc)&StartTask)){
@@ -100,75 +95,65 @@ void leibniz_task(void* pvParameters){
 			pi4 = pi4 +(1.0/zaehler);
 			zaehler += 2;
 			pi_l = pi4*4;	
-		//xEnd = xTaskGetTickCount();
-		//xDifference = xEnd - xStart;
-		
-	}
+		if (pi_l < pi_nachkomma){
+			Zeit_End = xTaskGetTickCount();	
+			Zeit_Difference = Zeit_End - Zeit_Start;
+			}
+		//if((xEventGroupGetBits(EventGroupPiCalc) & UebertragungsReset) == UebertragungsReset) {
+		 	//xEventGroupClearBits(EventGroupPiCalc, UebertragungsReset);
+		 	//pi_x = 0;
+		 	//}
+		//EventBits_t bits = xEventGroupGetBits(EventGroupPiCalc);
+		//if((bits & Datensperren) != 0) {
+		 		//xEventGroupSetBits(EventGroupPiCalc, daten_bereit);
+		 		//xEventGroupWaitBits(EventGroupPiCalc, daten_cleared, pdTRUE, pdFALSE, portMAX_DELAY);
+			//}
+		 //datenuebertragung = pi_x;
+		}
 	}
  }
-     //TickTypet xStart, xEnd, xDifference;
-    //for( ;; )
-    //{
-        //xStart = xTaskGetTickCount();
-        //vTaskDelay( pdMSTO_TICKS( 1000UL ) );
-        //xEnd = xTaskGetTickCount();
-        //xDifference = xEnd – xStart;
-        //printf( “Time diff: %lu ticksn”, xDifference );
-    //}
-
-		// else {
-		//if((xEventGroupGetBits(EventGroupPiCalc)&StartTask)){
-		//pi_l = 45621;
-	//}
-	//
-	//if xSemaphoreTake(MeinMutex,10/portTICK_RATE_MS) == pdTRUE {
-		//Datenuebertragung = pi_l;
-			//xSemaphoreGive(MeinMutex);
-		//}
-		//
-		//vTaskDelay(10/portTICK_RATE_MS);
-
-	//}
-	//xEventGroupClearBits(EventGroupPiCalc, StartTask);
-	// informiere_task();
-	 //warte_hier();
-// }
-//}
 
 void Nilakantha_task(void* pvParameters){
 	piN = 3;
 	uint32_t zaehler_s = 3;
-	//volatile TickType_t xTaskGetTickCount( void );
+	pi_nachkomma = 3.14160;
  for (;;) {	
+	 if((xEventGroupGetBits(EventGroupPiCalc)&ResetTask)){
+		piN = 0;
+		uint32_t zaehler_s = 3;
+		pi_n = 0;
+		} 
 	 if((xEventGroupGetBits(EventGroupPiCalc)&StartTask)){
 		 piN = piN+(4.0/(pow(zaehler_s,3)-zaehler_s));
 		 zaehler_s += 2;
 		 piN = piN-(4.0/(pow(zaehler_s,3)-zaehler_s));
 		 zaehler_s += 2;
-		 pi_n = piN;	
+		 pi_n = piN;
+		 if (pi_l < pi_nachkomma){
+			Zeit_End_n = xTaskGetTickCount();
+			Zeit_Difference_n = Zeit_End_n - Zeit_Start_n;
+		 	}	
+		 	//if((xEventGroupGetBits(EventGroupPiCalc) & UebertragungsReset) == UebertragungsReset) {
+		 		//xEventGroupClearBits(EventGroupPiCalc, UebertragungsReset);
+		 		//pi_x = 0;
+		 		//}
+		 	//EventBits_t bits = xEventGroupGetBits(EventGroupPiCalc);
+		 		//if((bits & Datensperren) != 0) {
+		 			//xEventGroupSetBits(EventGroupPiCalc, daten_bereit);
+		 			//xEventGroupWaitBits(EventGroupPiCalc, daten_cleared, pdTRUE, pdFALSE, portMAX_DELAY);
+					//}
+		 	//datenuebertragung = pi_x;	
 		}
 	 }
 }
-	 	//if((xEventGroupGetBits(EventGroupPiCalc) & UebertragungsReset) == UebertragungsReset) {
-			//xEventGroupClearBits(EventGroupPiCalc, UebertragungsReset);
-		 	//pi_x = 0;
-	 		//}
-	 	//EventBits_t bits = xEventGroupGetBits(EventGroupPiCalc);
-	 	//if((bits & Datensperren) != 0) {
-		 	//xEventGroupSetBits(EventGroupPiCalc, daten_bereit);
-		 	//xEventGroupWaitBits(EventGroupPiCalc, daten_cleared, pdTRUE, pdFALSE, portMAX_DELAY);
-	 	//}
-	 	//datenuebertragung = pi_x;
-	 	//vTaskDelay(10/portTICK_RATE_MS);
- 		//}
- 
-	// informiere_task();
-	 //warte_hier();
+
 
 
 
 void controllerTask(void* pvParameters) {
 	char pistring[12];
+	uint64_t pistring1[12];
+	uint64_t pistring2[12];
 	uint32_t fivehundertmillisecondscounter = 0;
 	initButtons();
 	for(;;) {
@@ -178,63 +163,46 @@ void controllerTask(void* pvParameters) {
 			//xEventGroupWaitBits(EventGroupPiCalc, daten_bereit, pdTRUE, pdFALSE, portMAX_DELAY);
 			//pi_x = datenuebertragung;
 			//xEventGroupClearBits(EventGroupPiCalc, Datensperren);
-			//xEventGroupSetBits(EventGroupPiCalc, daten_cleared);
-				if((xEventGroupGetBits(EventGroupPiCalc)&WechsleLeibniz)){
+			//xEventGroupSetBits(EventGroupPiCalc, daten_cleared);		
+			if((xEventGroupGetBits(EventGroupPiCalc)&WechsleLeibniz)){
 				vDisplayClear();
 				vDisplayWriteStringAtPos(0,0,"PI-Calc FS2022");
 				vDisplayWriteStringAtPos(1,0,"Leibniz-Algo");
 				sprintf(&pistring[0], "PI_L: %.8f", pi_l);
 				vDisplayWriteStringAtPos(2,0, "%s", pistring);
-				//vDisplayWriteStringAtPos(3,0, "Zeit: %s", xDifference);
-				//printf( “Time diff: %lu ticksn”, xDifference );
+				sprintf(&pistring1[0], "Zeit: %s", Zeit_Difference);
+				vDisplayWriteStringAtPos(3,0, "%s", pistring1);
 				} else {
 					vDisplayClear();
 					vDisplayWriteStringAtPos(0,0,"PI-Calc FS2022");
 					vDisplayWriteStringAtPos(1,0,"Nilakantha-Algo");
 					sprintf(&pistring[0], "PI_N: %.8f", pi_n);
 					vDisplayWriteStringAtPos(2,0, "%s", pistring);
+					sprintf(&pistring2[0], "Zeit: %s", Zeit_Difference_n);
+					vDisplayWriteStringAtPos(3,0, "%s", pistring2);
 				}
 				fivehundertmillisecondscounter = 50;
-			} else {
+			 } else {
 				fivehundertmillisecondscounter --;
 			}
-				//vDisplayClear();		
-		//vDisplayWriteStringAtPos(0,0, "MeineVariabe: %d",localy);		
-		//vTaskDelay(200/portTICK_RATE_MS);
+			
 		if(getButtonPress(BUTTON1) == SHORT_PRESSED) {
 			xEventGroupSetBits(EventGroupPiCalc, StartTask);
 			xEventGroupClearBits(EventGroupPiCalc, ResetTask);
-			xStart = xTaskGetTickCount();
+			Zeit_Start = xTaskGetTickCount();
+			Zeit_Start_n = xTaskGetTickCount();
 		}
 		if(getButtonPress(BUTTON2) == SHORT_PRESSED) {
 			xEventGroupSetBits(EventGroupPiCalc, StopTask);
 			xEventGroupClearBits(EventGroupPiCalc, StartTask);
 		}
-							//vDisplayClear();
-				//sprintf(&pistring[0], "PI_L: %.8f", pi_n);
-				//vDisplayWriteStringAtPos(0,0, "Nilakantha-Task");
-				//vDisplayWriteStringAtPos(1,0, "%s", pistring);			
-			//xEventGroupSetBits(EventGroupPiCalc,StartLeibniz);
-					//if (fivehundertmillisecondscounter == 0){
-			//vDisplayClear();
-			//sprintf(&pistring[0], "PI: %.8f", pi_l);
-			//vDisplayWriteStringAtPos(1,0, "%s", pistring);
-			//fivehundertmillisecondscounter = 50;
-		//} else {
-			//fivehundertmillisecondscounter --;
-		//}
-		//}
+
 		if(getButtonPress(BUTTON3) == SHORT_PRESSED) {
 			xEventGroupSetBits(EventGroupPiCalc, ResetTask);
 			xEventGroupClearBits(EventGroupPiCalc, StopTask);
 			xEventGroupClearBits(EventGroupPiCalc, StartTask);
 			xEventGroupClearBits(EventGroupPiCalc, WechsleLeibniz);
 			xEventGroupClearBits(EventGroupPiCalc, WechsleNilakantha);
-			pi_l = 0;
-			pi_n = 0;
-			vDisplayClear();
-			vDisplayWriteStringAtPos(4,0, "Reset");
-		//	vTaskDelay(10/portTICK_RATE_MS);
 		}
 		
 		if(getButtonPress(BUTTON4) == SHORT_PRESSED) {
@@ -262,5 +230,6 @@ void controllerTask(void* pvParameters) {
 		vTaskDelay(10/portTICK_RATE_MS);
 		}
 	}
+
 		
 	
